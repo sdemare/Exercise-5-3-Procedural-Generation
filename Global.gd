dec8f6aa3aabaee31e026150f054a8f024a82d6f
sdemare <sdemare@iu.edu>
2023-11-13 16:01:54 -0500

    Completes exercise

--- Global.gd
+++ Global.gd
@@ -1,5 +1,17 @@
 extends Node
 
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 func _unhandled_input(event):
 	if event.is_action_pressed("Menu"):
-		get_tree().quit()
+		var Menu = get_node_or_null("/root/Game/Menu")
+		if Menu == null:
+			get_tree().quit()
+		else:
+			if Menu.visible:
+				get_tree().paused = false
+				Menu.hide()
+			else:
+				get_tree().paused = true
+				Menu.show()
